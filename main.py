aaf65372f6311ee4cd466a9c26bccd6b5dff5119
Emm Kwami <edenated@gmail.com>
2022-06-02 20:20:53 -0700

    local

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
  
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, 
+    allow_origins=["*"], 
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"]
